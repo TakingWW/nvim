caa52ee03bf6007aec4118ff2cf8d2b7f1944b78
TakingWW <jub_camara@hotmail.com>
2023-07-03 19:11:11 -0300

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,22 +34,6 @@ local default_plugins = {
       require("nvterm").setup(opts)
     end,
   },
-
-  {
-    "NvChad/nvim-colorizer.lua",
-    init = function()
-      require("core.utils").lazy_load "nvim-colorizer.lua"
-    end,
-    config = function(_, opts)
-      require("colorizer").setup(opts)
-
-      -- execute colorizer as soon as possible
-      vim.defer_fn(function()
-        require("colorizer").attach_to_buffer(0)
-      end, 0)
-    end,
-  },
-
   {
     "nvim-tree/nvim-web-devicons",
     opts = function()
